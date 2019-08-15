0f255ce2ec6c1e79524f42e67d83b685fb17a62c
Gabriel Robinson-Leith <gabrielrobinsonleith@gmail.com>
2019-08-15 15:25:22 -0700

    updated new motor control for both speed nad movement, untested

--- stepper_motor_driver/src/main.cpp
+++ stepper_motor_driver/src/main.cpp
@@ -11,11 +11,20 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 
-int steppp=0;
+int revs=0;
+int size;
+String valStr;
+int val;
 
 
+byte byteRead;
+
+const byte numChars = 32;
+char receivedChars[numChars]; // an array to store the received data
+boolean newData = false;
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
 // Or, create it with a different I2C address (say for stacking)
 // Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61);
 
@@ -24,6 +33,58 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_StepperMotor *myMotor = AFMS.getStepper(400, 1);
 
 
+void recvWithEndMarker() {
+ static byte ndx = 0;
+ char endMarker = '\n';
+ char rc;
+ 
+ // if (Serial.available() > 0) {
+           while (Serial.available() > 0 && newData == false) {
+ rc = Serial.read();
+
+ if (rc != endMarker) {
+ receivedChars[ndx] = rc;
+ ndx++;
+ if (ndx >= numChars) {
+ ndx = numChars - 1;
+ }
+ }
+ else {
+ receivedChars[ndx] = '\0'; // terminate the string
+ ndx = 0;
+ newData = true;
+ }
+ }
+}
+
+void showNewData() {
+ if (newData == true) {
+   size = sizeof(receivedChars)/sizeof(receivedChars[0]);
+   char speed[size-1];
+
+  for(int i = 1; i<size; i++){
+    speed[i-1] = receivedChars[i];
+  }
+
+  valStr = String(speed);
+  val = valStr.toInt();
+
+   if (receivedChars[0] == 's'){
+    Serial.println("Speed: ");
+    Serial.println(val);
+   }
+   else if (receivedChars[0] == 'm'){ 
+     Serial.println("Move: ");
+     Serial.println(val);
+   }
+   else{
+     Serial.println("Error");
+   }
+
+ newData = false;
+ }
+}
+
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
 
@@ -31,28 +92,15 @@ void setup() {
   AFMS.begin();  // create with the default frequency 1.6KHz
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
 
-  myMotor->setSpeed(4000);  // 10 rpm
+  myMotor->setSpeed(0);  // 10 rpm
+
+   Serial.println("<Arduino is ready>");
 }
 
 
 
 
 void loop() {
-
-
-myMotor->step(200, BACKWARD, DOUBLE);
-steppp++;
-  Serial.print(steppp);
-  Serial.print(",");
-  Serial.println(analogRead(A0));
-        
-
-  }
-
-//void loop(){
-//  Serial.println(analogRead(A0));
-//}
-//
-//void loop() {
-// myMotor->step(200, FORWARD, DOUBLE);
-//}
+ recvWithEndMarker();
+ showNewData();
+}
